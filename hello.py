17d3c58c2bbddb8efa95b2fd59d4ce2be644890f
Prithwis Chatterjee <prithwis.chatterjee@cesc.co.in>
2025-03-03 23:34:20 +0530

    firts python

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hello Prithwis3333")
+num1 = 12
+num2 = 24
+sum = num1+num2
+print("sum ",sum)
---
+++ testSub.py
@@ -0,0 +1,19 @@
+print("test  ")
+inPutArray = [2,4,5,1,2,3,6,8]
+multiAppear = 0
+mismatch = 0
+arrLength = len(inPutArray)
+
+for x in range(arrLength):
+    for val in inPutArray:           
+        if val == x:
+            mismatch = x   
+            multiAppear = multiAppear+1
+        
+    if x!= mismatch:  
+        print("Missing ")          
+        print(x)
+    if multiAppear>1:
+        print("appears multiple time")
+        print(multiAppear)
+    multiAppear = 0
